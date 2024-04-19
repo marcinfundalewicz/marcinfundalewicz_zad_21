bb5274bca3cbb213fcae8b46479e06b4d8895a44
Marcin Fundalewicz <marcin.fundalewicz@gmail.com>
2024-04-19 14:38:31 +0200

    Stworzenie projektu

---
+++ src/main/java/pl/javastart/zadanie21/JjdInd84MarcinfundalewiczZad21Application.java
@@ -0,0 +1,13 @@
+package pl.javastart.zadanie21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjdInd84MarcinfundalewiczZad21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JjdInd84MarcinfundalewiczZad21Application.class, args);
+    }
+
+}
